02ec5dc4a3f93c317f84f2d53f0f53c47b233562
sqlm1 <sqlm1@users.noreply.github.com>
2026-05-03 17:36:18 -0400

    Restrict search indexing on all pages

--- server.js
+++ server.js
@@ -228,6 +228,7 @@ const types = {
   ".html": "text/html; charset=utf-8",
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
+  ".txt": "text/plain; charset=utf-8",
   ".png": "image/png",
   ".jpg": "image/jpeg",
   ".jpeg": "image/jpeg",
